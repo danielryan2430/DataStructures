15caeab9375b47037b86129b2c85b951204fdf02
Daniel Imberman <danielryan2430@gmail.com>
2017-03-24 09:41:52 -0700

    created basic DS analyzer. There is definitely changes that need to be made before it is released

--- cap1/datastructure_analyzer.py
+++ cap1/datastructure_analyzer.py
@@ -1,9 +1,6 @@
-import os
-
-
 class BigOAnalyzer(object):
     def __init__(self, data_structure):
-        self.data_structure = data_structure
+        self._data_structure = data_structure
 
     @property
     def data_structure(self):
@@ -13,10 +10,42 @@ class BigOAnalyzer(object):
     def data_structure(self, ds):
         self._data_structure = ds
 
-    def test_input(self,path):
-        file = os.open(path)
-        for i in file.getLines():
-            self.data_structure.insert(i)
+    # TODO I don't like that this class is directly reading from files.
+    # It should take in a list of lines and act accordingly
 
+    def test_input(self, path, num_lines):
+        f = open(path, 'r')
+        max_steps = 0
+        avg = 0
 
+        for i in range(0, num_lines):
+            curr_line = f.readline()
+            curr = self.data_structure.insert(curr_line)
+            max_steps = max(max_steps, curr)
+            avg = avg + curr
 
+        print "for inputting {} lines the average was {} and the max steps was {}".format(num_lines, max_steps, avg)
+
+    def test_lookup(self, path, num_lines):
+        f = open(path, 'r')
+        max_steps = 0
+        avg = 0
+        for i in range(0, num_lines):
+            curr_line = f.readline()
+            curr = self.data_structure.lookup(curr_line)
+            max_steps = max(max_steps, curr)
+            avg = avg + curr
+
+        print "for deleting {} lines the average was {} and the max steps was {}".format(num_lines, max_steps, avg)
+
+    def test_delete(self, path, num_lines):
+        f = open(path, 'r')
+        max_steps = 0
+        avg = 0
+        for i in range(0, num_lines):
+            curr_line = f.readline()
+            curr = self.data_structure.lookup(curr_line)
+            max_steps = max(max_steps, curr)
+            avg = avg + curr
+
+        print "for {} lines the average was {} and the max steps was {}".format(num_lines, max_steps, avg)
